889eb0c99a4756cec1a4988d445f6224e587c014
Roman Gulevich <r.gulevich@syberry.com>
2022-10-17 10:11:30 +0300

    Added implementation of creating Profile object by data from file.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws FileNotFoundException {
+        Map<String, String> map = new HashMap<>();
+
+        try (BufferedReader inputFile = new BufferedReader(new java.io.FileReader(file));) {
+            String s = inputFile.readLine();
+            while (s != null) {
+                String[] res = s.split("[:|\\n]");
+                map.put(res[0].trim(), res[1].trim());
+                s = inputFile.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(map.get("Name"), Integer.parseInt(map.get("Age")), map.get("Email"), Long.parseLong(map.get("Phone")));
     }
 }
